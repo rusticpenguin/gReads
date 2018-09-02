025367a18b292280c6e175ffce84e2ffd744fb1e
rusticpenguin <georgechios.music@gmail.com>
2018-09-02 14:51:59 -0600

    Delete button works again

--- src/App.js
+++ src/App.js
@@ -50,8 +50,11 @@ class App extends Component {
 
   onDismiss(id) {
     const isNotId = item => item.objectID !== id;
-    const updatedList = this.state.list.filter(isNotId);
-    this.setState({ list: updatedList})
+    const updatedHits = this.state.result.hits.filter(isNotId);
+    const updatedResult = Object.assign({}, this.state.result, updatedHits);
+    this.setState({
+      result: Object.assign({}, this.state.result, { hits: updatedHits })
+    });
   }
 
   render() {
