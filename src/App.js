0eda74098c40ebc58974ad83d9e238ed2ded4dfd
rusticpenguin <georgechios.music@gmail.com>
2018-09-02 14:44:56 -0600

    fetches from hackernews api

--- src/App.js
+++ src/App.js
@@ -1,42 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'http://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const DEFAULT_QUERY = 'redux';
 
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
 
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
 
 const isSearched = searchTerm => item =>
-    item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      list,
-      searchTerm: '',
-    }
-    
+      result: null,
+      searchTerm: DEFAULT_QUERY,
+    };
+
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.logThis = this.logThis.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  setSearchTopStories(result) {
+    this.setState({ result });
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+    .then(res => res.json())
+    .then(res => this.setSearchTopStories(res))
+    .catch(err => err);
   }
 
   logThis(data) {
@@ -54,7 +55,10 @@ class App extends Component {
   }
 
   render() {
-    const { searchTerm, list } = this.state;
+    const { searchTerm, result } = this.state;
+
+    if (!result) { return null; }
+
     return (
       <div className="page">
         <div className="interactions">
@@ -65,7 +69,7 @@ class App extends Component {
             Search
           </Search>
           <Table 
-            list = {list}
+            list = {result.hits}
             pattern = {searchTerm}
             onDismiss = {this.onDismiss}
           />
@@ -75,7 +79,7 @@ class App extends Component {
   }
 }
 
-const Search = ({ value, onChange, children }) => 
+const Search = ({ value, onChange, children }) =>
     <form>
       {children} <input 
         type="text"
