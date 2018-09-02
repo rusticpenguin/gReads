d05068d4e0b0931eda25329c068c98da34cc953c
rusticpenguin <georgechios.music@gmail.com>
2018-09-02 11:43:44 -0600

    Search functionality

--- src/App.js
+++ src/App.js
@@ -33,21 +33,31 @@ const list = [
   },
 ];
 
+const isSearched = searchTerm => item =>
+    item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       list,
+      searchTerm: '',
     }
     
     this.logThis = this.logThis.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
   }
 
   logThis(data) {
     console.log(data);
   }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
   onDismiss(id) {
     const isNotId = item => item.objectID !== id;
     const updatedList = this.state.list.filter(isNotId);
@@ -57,7 +67,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.list.map(item =>
+        <form>
+          <input type="text" onChange={this.onSearchChange} />
+        </form>
+        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
             <div key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>
